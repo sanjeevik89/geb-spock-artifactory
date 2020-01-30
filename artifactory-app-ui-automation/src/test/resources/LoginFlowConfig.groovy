1f625d5f27f9ae640415ac46f4ac1fba59427b4b
Sanjeevi Kumar <sanjeevik89@gmail.com>
2020-01-31 00:35:27 +0530

    test config for success & failuew

--- artifactory-app-ui-automation/src/test/resources/LoginFlowConfig.groovy
+++ artifactory-app-ui-automation/src/test/resources/LoginFlowConfig.groovy
@@ -1,29 +1,21 @@
-/*
- * Copyright (c) 2018. https://automationschool.com
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 environments {
-    test {
-		//For the username, do not use the email but the artifactory user without the @ sign in the front.
+    test_success {
+		//testing happy path.
         username = "admin"
         password = "ULcht3WGeHKCWqR"
+        search_artifact = "Bookstore.war"
+    }
+
+    test_failure {
+        username = "dummytestuser"
+        password = "dummytestpassword"
+        search_artifact = "test.jar"
     }
 
     prod {
 		//For the username, do not use the email but the artifactory user without the @ sign in the front.
         username = "dummyproduser"
         password = "dummyprodpassword"
+        search_artifact = "test.war"
     }
 }
