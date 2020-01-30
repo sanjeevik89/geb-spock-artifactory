e8cbf450ffb1a217df0a0f9788d6320af378b4b0
Sanjeevi Kumar <sanjeevik89@gmail.com>
2020-01-30 22:08:52 +0530

    using full JS path instead of class

--- artifactory-app-ui-automation/src/test/groovy/modules/HeaderModule.groovy
+++ artifactory-app-ui-automation/src/test/groovy/modules/HeaderModule.groovy
@@ -23,12 +23,12 @@ class HeaderModule extends Module {
         navBar (required: true) {$("nav#jf-header")}
         navBarRight (required: true) { navBar.find('div.navbar-right')}
         currentUserDropdownArrow (required: true, wait: true) { navBarRight.find('i.action-icon.icon.icon-small-arrow-down')}
-        currentUserDropdown (required: false, wait: true) { navBarRight.find('dropdown-menu.dropdown-menu-right.actions-dropdown.text-left') }
-        logoutButton (required: false, wait: true) { currentUserDropdown.find('li.action-item.dynamic-action.ng-scope.menu-item-icon-logout')}
+        currentUserDropdown (required: true, wait: true) { navBarRight.find('dropdown-menu.dropdown-menu-right.actions-dropdown.text-left') }
+        logoutButton (required: false, wait: true) { $("#jf-header > div.navbar-right > div.pull-right.header-section.ng-scope.quick-actions > div > div > jf-actions > ul > li > span > ul > li.action-item.dynamic-action.ng-scope.menu-item-icon-logout")}
     }
 
     def clickOnLogout(){
-        currentUserDropdown.click()
+        currentUserDropdownArrow.click()
         logoutButton.click()
     }
 }
